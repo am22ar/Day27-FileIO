86cc4d41f859495576a12bcd72c5224a0174f4c6
am22ar <amarpatil12222@gmail.com>
2022-11-11 20:56:16 +0530

    Added: Read data from employee payroll File

--- src/main/java/com/fileio/EmployeePayrollService.java
+++ src/main/java/com/fileio/EmployeePayrollService.java
@@ -23,23 +23,15 @@ import java.util.List;
 import static java.nio.file.StandardWatchEventKinds.*;
 
 public class EmployeePayrollService {
-
-	public void listFilesDirectory() throws IOException, InterruptedException {
-		ArrayList<String> writing = new ArrayList<String>();
-
-		writing.add("Id : 02");
-		writing.add("Name : Amar");
-		writing.add("Salary : 150000");
-//		File Exist => Override                        
-		// Will create new file if not exist
-		Files.write(Paths.get(
-				"C:\\Users\\Amar\\Desktop\\RFP-Java-Basics\\FileIO\\src\\main\\java\\com\\fileio\\TestDemo.txt"),
-				writing, StandardOpenOption.CREATE);
-		System.out.println("Exporting Into File Is Done...");
-	}
-
+	public void listFilesDirectory() throws IOException, InterruptedException  {
+        
+		List<String> line=	Files.readAllLines(Paths.get("C:\\Users\\Amar\\Desktop\\RFP-Java-Basics\\FileIO\\src\\main\\java\\com\\fileio\\TestDemo.txt"));
+				for(String lines:line) {
+					System.out.println(lines);
+				}
+		}
 	public static void main(String[] args) throws IOException, InterruptedException {
-		EmployeePayrollService obj = new EmployeePayrollService();
+		EmployeePayrollService obj=new EmployeePayrollService();
 		obj.listFilesDirectory();
 	}
 }
