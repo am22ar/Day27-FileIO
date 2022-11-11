070346f414a223e2b3bbb8384eaec0953108721c
am22ar <amarpatil12222@gmail.com>
2022-11-11 21:00:51 +0530

    Added: count number of entries

--- src/main/java/com/fileio/EmployeePayrollService.java
+++ src/main/java/com/fileio/EmployeePayrollService.java
@@ -23,15 +23,19 @@ import java.util.List;
 import static java.nio.file.StandardWatchEventKinds.*;
 
 public class EmployeePayrollService {
+	int count=0;
 	public void listFilesDirectory() throws IOException, InterruptedException  {
         
 		List<String> line=	Files.readAllLines(Paths.get("C:\\Users\\Amar\\Desktop\\RFP-Java-Basics\\FileIO\\src\\main\\java\\com\\fileio\\TestDemo.txt"));
+		
 				for(String lines:line) {
+					count++;
 					System.out.println(lines);
 				}
 		}
 	public static void main(String[] args) throws IOException, InterruptedException {
 		EmployeePayrollService obj=new EmployeePayrollService();
 		obj.listFilesDirectory();
+		System.out.print(obj.count);
 	}
 }
